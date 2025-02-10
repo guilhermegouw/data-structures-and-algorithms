e2877b87fa61ac02107f0c62cca5c69543b7087d
guilhermegouw <guilherme.gouw@gmail.com>
2025-02-10 13:41:07 -0300

    unfinished challenge...

---
+++ python/problems/arrays/find_contiguous_subarray_with_sum/challenge.py
@@ -0,0 +1,31 @@
+"""
+Challenge: Find Contiguous Subarray With Sum
+Level: Medium
+
+Write a function that takes:
+
+    An array of integers (arr)
+    A target sum (target)
+
+The function should return a tuple with the starting and ending indices
+(start, end) of a contiguous subarray where the sum of its elements equals the
+target sum.
+
+If no such subarray exists, return None.
+
+Input 1:
+    arr = [1, 2, 3]
+    target = 10
+Output 1:
+    None
+
+
+Input 2:
+    arr = [1, 2, 3, 7, 5]
+    target = 12
+
+Output 2:
+    (1, 3)
+
+Note: subarray [2, 3, 7] -> 2 + 3 + 7 = 12
+"""
