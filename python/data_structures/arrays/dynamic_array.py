9ec6547f05f925bd1a2e25db8413882833117da6
guilhermegouw <guilherme.gouw@gmail.com>
2025-04-05 08:43:35 -0300

    2025-04-05

--- python/data_structures/arrays/dynamic_array.py
+++ python/data_structures/arrays/dynamic_array.py
@@ -8,15 +8,25 @@ class DynamicArray:
         self.size = 0
         self.growth_factor = 2
 
-    def append(self, value):
+    def __getitem__(self, index):
+        self._validate_index(index)
+        return self.internal_array[index]
+
+    def __setitem__(self, index, value):
+        self._validate_index(index)
+        self._validate_type(value)
+        self.internal_array[index] = value
+
+    def _validate_index(self, index):
+        if index not in range(self.size):
+            raise IndexError("Index out of bounds")
+
+    def _validate_type(self, value):
         if not isinstance(value, self.data_type):
             raise TypeError(
-                f"Expected type {self.data_type}, got {type(value)}"
+                f"DynamicArray expect type: {self.data_type}"
+                + f" but got: {type(value)}"
             )
-        if self.size == self.capacity:
-            self._resize()
-        self.internal_array[self.size] = value
-        self.size += 1
 
     def _resize(self):
         new_array = [None] * (self.capacity * self.growth_factor)
@@ -24,3 +34,28 @@ class DynamicArray:
             new_array[i] = self.internal_array[i]
         self.internal_array = new_array
         self.capacity *= self.growth_factor
+
+    def append(self, value):
+        self._validate_type(value)
+        if self.size == self.capacity:
+            self._resize()
+        self.internal_array[self.size] = value
+        self.size += 1
+
+    def insert(self, index, value):
+        self._validate_type(value)
+        if index < 0 or index > self.size:
+            raise IndexError("Index out of bounds")
+        if self.size == self.capacity:
+            self._resize()
+        for i in range(self.size, index, -1):
+            self.internal_array[i] = self.internal_array[i - 1]
+        self.internal_array[index] = value
+        self.size += 1
+
+    def delete(self, index):
+        self._validate_index(index)
+        for i in range(self.size - 1):
+            self.internal_array[i] = self.internal_array[i + 1]
+        self.internal_array[self.size - 1] = None
+        self.size -= 1
--- python/data_structures/arrays/test_dynamic_array.py
+++ python/data_structures/arrays/test_dynamic_array.py
@@ -21,7 +21,9 @@ def test_append():
     assert array.internal_array == [1, None]
     assert array.size == 1
     with pytest.raises(
-        TypeError, match="Expected type <class 'int'>, got <class 'str'>"
+        TypeError,
+        match="DynamicArray expect type: <class 'int'>"
+        + " but got: <class 'str'>",
     ):
         array.append("one")
 
@@ -33,3 +35,50 @@ def test_resize():
     array.append(3)
     assert array.capacity == 4
     assert array.internal_array[:3] == [1, 2, 3]
+
+
+def test_retrieve():
+    array = DynamicArray(int)
+    array.append(1)
+    array.append(2)
+    array.append(3)
+    assert array[0] == 1
+    with pytest.raises(IndexError, match="Index out of bounds"):
+        array[3]
+
+
+def test_update():
+    array = DynamicArray(int)
+    array.append(1)
+    array.append(2)
+    array.append(3)
+    array[0] = 4
+    assert array[0] == 4
+    with pytest.raises(IndexError, match="Index out of bounds"):
+        array[3] = 5
+    with pytest.raises(
+        TypeError,
+        match="DynamicArray expect type: <class 'int'> but got: <class 'str'>",
+    ):
+        array[0] = "One"
+
+
+def test_insert():
+    array = DynamicArray(str)
+    array.append("apple")
+    array.append("banana")
+    array.insert(1, "strawberry")
+    assert array.internal_array[: array.size] == [
+        "apple",
+        "strawberry",
+        "banana",
+    ]
+
+
+def test_delete():
+    array = DynamicArray(int)
+    array.append(1)
+    array.append(2)
+    array.append(3)
+    array.delete(0)
+    assert array.internal_array[: array.size] == [2, 3]
